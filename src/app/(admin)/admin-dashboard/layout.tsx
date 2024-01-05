3a2ca20a6dbddcfe27474dc2c435de2225f756d5
Basel <baselnajjar2166@gmail.com>
2024-01-06 01:22:23 +0300

    building the main layout , the CRUD layout and the Roles section and website Theme

--- src/app/(admin)/admin-dashboard/layout.tsx
+++ src/app/(admin)/admin-dashboard/layout.tsx
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
 import Sidebar from "@/components/Pars/Sidebar";
 import Topbar from "@/components/Pars/Topbar";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function DashboardLayout({
+   children,
+}: {
+   children: React.ReactNode;
+}) {
    return (
       <section className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] min-h-screen relative">
          <Sidebar />
@@ -10,27 +13,4 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
          {children}
       </section>
    );
-=======
-import Sidbar from '@/components/pars/Sidbar';
-import Topbar from '@/components/pars/Topbar';
-import React from 'react';
-
-
-const Layout = ({ children,}: {children: React.ReactNode}) => {
-
-  return (
-    
-    <div>
-      <div><Topbar/></div>
-      <div>{children}</div>
-      <div> <Sidbar/></div>
-    </div>
-    
-  );
-
->>>>>>> b5c4263dce3b346334264401b16680622b6a71a7
-};
-  
-
-
-export default Layout;
+}
