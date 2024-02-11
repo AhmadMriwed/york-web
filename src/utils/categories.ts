ea6dd1c20bc8e62f46af3ae8a0598b9cc7dddbce
AdelMohammed8 <adelmohm209@gmail.com>
2024-02-10 20:59:48 -0800

    update utils

---
+++ src/utils/categories.ts
@@ -0,0 +1,25 @@
+
+
+export const categories = [
+    {
+        title: 'Science',
+        value: 'Science',
+        image: ""
+    },
+    {
+        title: 'Technology',
+        value: 'Technology',
+        image: ""
+    },
+    {
+        title: 'Physics',
+        value: 'Physics',
+        image: ""
+    },
+    {
+        title: 'Literature',
+        value: 'Literature',
+        image: ""
+    }
+
+]
