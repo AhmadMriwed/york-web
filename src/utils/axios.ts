362be730975f045c36e2766f329339836ff30f63
Ahmad Ebrahim <113711103+ahmad-ebrahim-1@users.noreply.github.com>
2024-09-17 18:50:02 +0300

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,15 +3,21 @@ import { baseURL } from "./api";
 import Cookie from "universal-cookie";
 
 const cookie = new Cookie();
-let adminToken = cookie.get("admin_token");
 
 export const Axios = axios.create({
   baseURL: baseURL,
-  headers: {
-    // Authorization: `Bearer 78|3PzjMYbKuGOdZ8HNQGHprPwp1lPyQEP6xlLkdAGK4f63e1cc`,
-    Authorization: `Bearer ${adminToken}`,
-  },
 });
+
+Axios.interceptors.request.use(
+  (config) => {
+    const adminToken = cookie.get("admin_token");
+    if (adminToken) {
+      config.headers.Authorization = `Bearer ${adminToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export const UserAxios = axios.create({
   baseURL: baseURL,
