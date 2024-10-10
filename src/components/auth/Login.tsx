2fb4a344aec471f50958ccfa8f8b922f428c5237
ahmad-ebrahim-1 <ahmadeb911@gmail.com>
2024-10-10 15:45:28 +0300

    fix login bug

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -125,165 +125,173 @@ const Login = ({ userType }: { userType: string }) => {
   }));
 
   return (
-    <div className="min-w-[100vw] min-h-[100dvh] overflow-hidden">
-      <Image
-        src={userType === "admin" ? "/adminlogin.png" : "/userlogin.png"}
-        alt="background image"
-        fill
-        className="object-cover z-[-1]"
-      />
-      <div className="w-full h-full absolute top-0 left-0 bg-[rgba(0,212,212,0.58)] mix-blend-color z-[-1]"></div>
+    <div
+      className="min-w-[100vw] min-h-[100vh]"
+      style={{
+        backgroundImage:
+          userType === "admin" ? "url(/adminlogin.png)" : "url(/userlogin.png)",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div
+        className="w-full h-full"
+        style={{ backgroundColor: "rgba(0,210,210,0.6)" }}
+      >
+        <Reveal triggerOnce duration={1000}>
+          <div className="flex items-start justify-between md:px-8 md:py-4">
+            <div className="hidden md:block">
+              <Image src="/logo.png" alt="logo" width={75} height={75} />
+            </div>
 
-      <Reveal triggerOnce duration={1000}>
-        <div className="min-h-[100dvh] min-w-[100vw] flex items-start justify-between px-8 py-4">
-          <div className="hidden md:block">
-            <Image src="/logo.png" alt="logo" width={100} height={100} />
-          </div>
-
-          <div
-            className="flex items-center justify-center absolute w-full h-full top-0 right-0  md:top-[50%] md:right-[10.75rem] md:translate-y-[-50%]
-              md:w-[500px] md:h-[calc(100vh-2rem)] md:rounded-[9px] bg-[rgba(19,24,30,0.9)]"
-          >
-            <div className="flex flex-col items-center w-[calc(100%-4rem)] h-[calc(100%-4rem)] md:py-8 md:px-4 text-[#FFF]">
-              <div className="md:hidden pb-4">
-                <Image src="/logo.png" alt="logo" width={100} height={100} />
-              </div>
-              <span className="text-base text-xl md:text-2xl mb-1.5 tracking-widest">
-                welcome to
-              </span>
-              <p className="text-3xl sm:text-3xl md:text-4xl font-semibold mb-10">
-                York British Academy
-              </p>
-
-              <form
-                onSubmit={formik.handleSubmit}
-                className="grid w-full costum_form"
-              >
-                {userType === "admin" && <ShowError error={adminError} />}
-                {userType === "trainer" && <ShowError error={trainerError} />}
-                {userType === "user" && <ShowError error={userError} />}
-
-                <span className="text-base tracking-widest mb-4">
-                  Welcome Back!
-                </span>
-                {formik.touched.email && formik.errors.email && (
-                  <p className="error-mesage">{formik.errors.email}</p>
-                )}
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter Your Email"
-                  className={`login-input mb-4 ${
-                    formik.errors.email
-                      ? "!border-[var(--secondary-color-red)]"
-                      : ""
-                  }`}
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                />
-                {formik.touched.password && formik.errors.password && (
-                  <p className="error-mesage">{formik.errors.password}</p>
-                )}
-                <div className="w-full relative">
-                  <div
-                    className="absolute top-[50%] translate-y-[-95%] right-3.5 cursor-pointer"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? (
-                      <EyeCloseIcon style={{ fontSize: "16px" }} />
-                    ) : (
-                      <VisibleIcon style={{ fontSize: "16px" }} />
-                    )}
+            <div className="flex items-center justify-center w-full min-h-[100dvh] md:mr-[10rem] md:w-[500px] md:min-h-[calc(100vh-2rem)] md:rounded-[9px] bg-[rgba(19,24,30,0.9)]">
+              <div className="flex flex-col items-center justify-between w-[calc(100%-4rem)] h-[calc(100%-4rem)] md:py-8 md:px-4 text-[#FFF]">
+                <div className="flex flex-col items-center">
+                  <div className="md:hidden pb-4">
+                    <Image
+                      src="/logo.png"
+                      alt="logo"
+                      width={100}
+                      height={100}
+                    />
                   </div>
+                  <span className="text-base text-xl md:text-2xl mb-1.5 tracking-widest">
+                    welcome to
+                  </span>
+                  <p className="text-3xl sm:text-3xl md:text-4xl font-semibold mb-10">
+                    York British Academy
+                  </p>
+                </div>
+
+                <form onSubmit={formik.handleSubmit} className="w-full grid">
+                  {userType === "admin" && <ShowError error={adminError} />}
+                  {userType === "trainer" && <ShowError error={trainerError} />}
+                  {userType === "user" && <ShowError error={userError} />}
+
+                  <span className="text-base tracking-widest mb-4">
+                    Welcome Back!
+                  </span>
+                  {formik.touched.email && formik.errors.email && (
+                    <p className="error-mesage">{formik.errors.email}</p>
+                  )}
                   <input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    name="password"
-                    placeholder="Enter Your Password"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Your Email"
                     className={`login-input mb-4 ${
-                      formik.errors.password
+                      formik.errors.email
                         ? "!border-[var(--secondary-color-red)]"
                         : ""
                     }`}
-                    value={formik.values.password}
+                    value={formik.values.email}
                     onChange={formik.handleChange}
                   />
-                </div>
-
-                <Link
-                  href={
-                    userType === "admin"
-                      ? "/admin/login/recover-password"
-                      : userType === "trainer"
-                      ? "/trainer/login/recover-password"
-                      : "/user/login/recover-password"
-                  }
-                  className="justify-self-end hover:no-underline"
-                >
-                  <span className="text-md leading-8 text-[#16FACD]">
-                    Forgot Your Password?
-                  </span>
-                </Link>
-
-                {userType !== "admin" && (
-                  <div className="my-4">
-                    <GoogleOAuthProvider clientId="507710031458-l9ir69lm854cg4ag6bfsumsneh6mg1s1.apps.googleusercontent.com">
-                      <GoogleLogin
-                        onSuccess={(credentialResponse) => {
-                          console.log(credentialResponse);
-                          let token = credentialResponse.credential;
-                          try {
-                            axios
-                              .post(
-                                `https://cms.yorkacademy.uk/api/google/callback?token=${token}`
-                              )
-                              .then((res) => {
-                                console.log(res);
-                              });
-                          } catch (error) {
-                            console.log(error);
-                          }
-                        }}
-                        onError={() => {
-                          console.log("Login Failed");
-                        }}
-                      />
-                    </GoogleOAuthProvider>
-                  </div>
-                )}
-
-                <button type="submit" className="colored-btn !text-[16px] mt-4">
-                  {userType === "admin" &&
-                    (adminLoading ? <Loader size="sm" /> : <>Sign in</>)}
-                  {userType === "trainer" &&
-                    (trainerLoading ? <Loader size="sm" /> : <>Sign in</>)}
-                  {userType === "user" &&
-                    (userLoading ? <Loader size="sm" /> : <>Sign in</>)}
-                </button>
-
-                {userType !== "admin" && (
-                  <p className="w-full flex items-center justify-center mt-6">
-                    Not a Member?
-                    <Link
-                      href={
-                        userType === "trainer"
-                          ? "/trainer/signup"
-                          : "/user/signup"
-                      }
-                      className="text-[#16FACD] hover:text-[#16FACD] ml-1.5"
+                  {formik.touched.password && formik.errors.password && (
+                    <p className="error-mesage">{formik.errors.password}</p>
+                  )}
+                  <div className="w-full relative">
+                    <div
+                      className="absolute top-[50%] translate-y-[-80%] right-3.5 cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
                     >
-                      Signup
-                    </Link>
-                  </p>
-                )}
+                      {showPassword ? (
+                        <EyeCloseIcon style={{ fontSize: "16px" }} />
+                      ) : (
+                        <VisibleIcon style={{ fontSize: "16px" }} />
+                      )}
+                    </div>
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      placeholder="Enter Your Password"
+                      className={`login-input mb-2 ${
+                        formik.errors.password
+                          ? "!border-[var(--secondary-color-red)]"
+                          : ""
+                      }`}
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                    />
+                  </div>
 
+                  <Link
+                    href={
+                      userType === "admin"
+                        ? "/admin/login/recover-password"
+                        : userType === "trainer"
+                        ? "/trainer/login/recover-password"
+                        : "/user/login/recover-password"
+                    }
+                    className="justify-self-end hover:no-underline"
+                  >
+                    <span className="text-md text-[#16FACD]">
+                      Forgot your password?
+                    </span>
+                  </Link>
+
+                  {userType !== "admin" && (
+                    <div className="mt-5 mb-2">
+                      <GoogleOAuthProvider clientId="507710031458-l9ir69lm854cg4ag6bfsumsneh6mg1s1.apps.googleusercontent.com">
+                        <GoogleLogin
+                          onSuccess={(credentialResponse) => {
+                            console.log(credentialResponse);
+                            let token = credentialResponse.credential;
+                            try {
+                              axios
+                                .post(
+                                  `https://cms.yorkacademy.uk/api/google/callback?token=${token}`
+                                )
+                                .then((res) => {
+                                  console.log(res);
+                                });
+                            } catch (error) {
+                              console.log(error);
+                            }
+                          }}
+                          onError={() => {
+                            console.log("Login Failed");
+                          }}
+                        />
+                      </GoogleOAuthProvider>
+                    </div>
+                  )}
+
+                  <button
+                    type="submit"
+                    className="colored-btn !text-[16px] mt-4"
+                  >
+                    {userType === "admin" &&
+                      (adminLoading ? <Loader size="sm" /> : <>Sign in</>)}
+                    {userType === "trainer" &&
+                      (trainerLoading ? <Loader size="sm" /> : <>Sign in</>)}
+                    {userType === "user" &&
+                      (userLoading ? <Loader size="sm" /> : <>Sign in</>)}
+                  </button>
+
+                  {userType !== "admin" && (
+                    <p className="w-full flex items-center justify-center mt-6">
+                      Not a Member?
+                      <Link
+                        href={
+                          userType === "trainer"
+                            ? "/trainer/signup"
+                            : "/user/signup"
+                        }
+                        className="text-[#16FACD] hover:text-[#16FACD] ml-1.5"
+                      >
+                        Signup
+                      </Link>
+                    </p>
+                  )}
+                </form>
                 <div
                   style={{
                     width: 150,
-                    position: "absolute",
-                    bottom: 15,
-                    right: 15,
+                    alignSelf: "end",
+                    paddingBlock: "2rem",
                   }}
                 >
                   <SelectPicker
@@ -297,11 +305,11 @@ const Login = ({ userType }: { userType: string }) => {
                     onChange={() => {}}
                   />
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-      </Reveal>
+        </Reveal>
+      </div>
     </div>
   );
 };
