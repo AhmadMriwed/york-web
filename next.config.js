13ca54c8b60292546c14975b09069a064c63252c
Omran Alrbedan <om.alrbedan100@gmail.com>
2025-03-13 00:05:29 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+const createNextIntlPlugin = require('next-intl/plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -59,4 +59,4 @@ const nextConfig = {
 const withNextIntl = createNextIntlPlugin();
 
 // تصدير الإعدادات النهائية
-export default withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig);
