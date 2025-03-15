0618c62a8f96f9fb53b706bbdc6b300b1a4f3969
Omran Alrbedan <om.alrbedan100@gmail.com>
2025-03-15 06:34:42 -0700

    update

--- src/app/(root)/[locale]/layout.tsx
+++ src/app/(root)/[locale]/layout.tsx
@@ -3,11 +3,11 @@ import { Conversation } from "@/components/review/conversation";
 import { Toaster } from "sonner";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { routing } from "@/i18n/routing";
+
 import Navbar from "@/components/navbar/Navbar";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import Footer from "@/components/footer/Footer";
+import { getServerLanguage } from "./api/getServerLanguage";
 
 export default async function RootLayout({
   children,
@@ -16,11 +16,7 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params;
-  console.log(locale);
-  if (!routing.locales.includes(locale as any)) {
-    notFound();
-  }
+  const locale = getServerLanguage();
 
   const messages = await getMessages();
 
