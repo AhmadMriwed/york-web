f4c4f58cbef8e0f3c6da17644109d5bc88d0daf8
Omran Alrbedan <om.alrbedan100@gmail.com>
2025-04-16 03:17:58 -0700

    update

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -4,12 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { usePathname, useRouter } from "next/navigation";
 import { GlobalState } from "@/types/storeTypes";
 import { getUserProfile } from "@/store/userStore/slices/userSlice";
+import Cookie from "universal-cookie";
+import { getAdminProfile } from "@/store/adminstore/slices/authSlice";
+const cookie = new Cookie();
 
 const TrainerLayout = ({ children }: PropsWithChildren) => {
   const dispatch: any = useDispatch();
   const router = useRouter();
   const pathName: string = usePathname();
   const { user } = useSelector((state: GlobalState) => state.userSlice);
+
+  useEffect(() => {
+    const token = cookie.get("admin_token");
+    if (token) {
+      dispatch(getAdminProfile());
+    }
+  }, []);
 
   useEffect(() => {
     if (!user)
